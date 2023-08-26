330e5074edd8956e45ac6d3372900e94cb4e015a
Kay Bahr <124631379+KayBahr@users.noreply.github.com>
2023-08-26 00:53:16 +0000

    shop structure commit

--- src/front/js/pages/shop.js
+++ src/front/js/pages/shop.js
@@ -7,7 +7,30 @@ export const Shop = () => {
 
 	return (
 		<div className="shop-container">
-			Shop
+			<h2>Browse Items</h2>
+			<ul className="nav nav-underline" id="myTab" role="tablist">
+				<li className="nav-item" role="presentation">
+					<button className="nav-link active" id="all-tab" data-bs-toggle="tab" data-bs-target="#all" type="button" role="tab" aria-controls="all" aria-selected="true">All items</button>
+				</li>
+				<li className="nav-item" role="presentation">
+					<button className="nav-link" id="stickers-tab" data-bs-toggle="tab" data-bs-target="#stickers" type="button" role="tab" aria-controls="stickers" aria-selected="false">Stickers</button>
+				</li>
+				<li className="nav-item" role="presentation">
+					<button className="nav-link" id="jewelry-tab" data-bs-toggle="tab" data-bs-target="#jewelry" type="button" role="tab" aria-controls="jewelry" aria-selected="false">Jewelry</button>
+				</li>
+			</ul>
+
+			<div class="tab-content">
+				<div class="tab-pane active" id="all" role="tabpanel" aria-labelledby="all-tab" tabindex="0">
+					All items
+				</div>
+				<div class="tab-pane" id="stickers" role="tabpanel" aria-labelledby="stickers-tab" tabindex="0">
+					Stickers
+				</div>
+				<div class="tab-pane" id="jewelry" role="tabpanel" aria-labelledby="jewelry-tab" tabindex="0">
+					Jewelry
+				</div>
+			</div>
 		</div>
 	);
 };
